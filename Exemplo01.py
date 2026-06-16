1015c913aae95b349f53bbc89842657eb2395a5b
gedianfunka <gediangabrielf@gmail.com>
2026-06-16 08:26:27 -0300

    exemplo01

---
+++ Exemplo01.py
@@ -0,0 +1,37 @@
+idade = 18
+
+altura = 1.75
+
+nome = "João"
+
+aprovado = True
+
+frutas = ["maça", "banana", "uva"]
+
+cores  = ("azul", "verde", "branco")
+
+aluno = {
+    "nome": "Gedian",
+    "idade": 18,
+    "curso": "Desenvolvimento de sistemas"
+}
+
+print("Exemplos de tipos de dados de Python\n")
+
+print("Idade = ", idade)
+print("Tipo: ", type(idade))
+
+print("\nAltura: ", altura)
+print("Tipo: ", type(altura))
+
+print("\nAprovado: ", aprovado)
+print("Tipo: ", type(aprovado))
+
+print("\nFrutas: ", frutas)
+print("Tipo: ", type(frutas))
+
+print("\nCores: ", cores)
+print("Tipo: ", type(cores))
+
+print("\nAluno: ", aluno)
+print("Tipo: ", type(aluno))
