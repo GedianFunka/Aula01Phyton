0ef93d16812a8a64554e1fd855c02b965f9a9ff3
gedianfunka <gediangabrielf@gmail.com>
2026-08-13 10:29:12 -0300

    Atividade Banco de Dados POO

---
+++ ConectandoBancoDadosPOO/Models.py
@@ -0,0 +1,28 @@
+from Exemplo01Py.Exemplo03 import quantidade
+
+
+class Produto:
+    def __init__(self,nome, preco, quantidade, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+        self.categoria = categoria
+
+    def comverter_tupla(self):
+        return (self.nome, self.preco, self.quantidade, self.categoria)
+
+    @staticmethod
+    def reverte_tupla(tupla):
+        produto = Produto(
+            nome = tupla[1],
+            preco = tupla[2],
+            quantidade = tupla[3],
+            categoria = tupla[4]
+        )
+
+        produto.id = tupla[0]
+        return produto
+
+    def exibir(self):
+        print(f"{self.id} | {self.nome} | {self.preco} | {self.quantidade} | {self.categoria}")
+
